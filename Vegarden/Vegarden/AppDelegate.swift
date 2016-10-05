c79301f3c7d06f10d4d01400b5fb77d49b27a23c
Sarah Cleland <sarah.c.cleland@gmail.com>
2016-10-05 22:05:35 +1030

    one gitignore file and fixes for the VCs

--- Vegarden/Vegarden/AppDelegate.swift
+++ Vegarden/Vegarden/AppDelegate.swift
@@ -55,12 +55,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     // MARK: - Split view
     
     func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
+        
         guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-        if topAsDetailController.detailItem == nil {
+        guard (secondaryAsNavController.topViewController as? CommonViewController) != nil else { return false }
+//  /      if topAsDetailController.detailItem == nil {
             // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-            return true
-        }
+//            return true
+//       ` }
         return false
     }
     
