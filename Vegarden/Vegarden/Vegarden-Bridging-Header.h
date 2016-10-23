ab38d2ff80a3900c9de6c7fccba71ecc74937d8f
Sarah Cleland <sarah.c.cleland@gmail.com>
2016-10-24 00:37:27 +1030

    Added RingProgressBar, y GHContextualMenu

--- Vegarden/Vegarden/Vegarden-Bridging-Header.h
+++ Vegarden/Vegarden/Vegarden-Bridging-Header.h
@@ -5,7 +5,7 @@
 #import  "Popup.h"
 //#define MR_SHORTHAND
 #import "NSManagedObject+Duplicate.h"
-//#import "SMTabBar.h"
+#import "GHContextMenuView.h"
 //#import "SMMasterViewController.h"
 //#import "SMDetailViewController.h"
 //#import "SMTabBarItemCell.h"
